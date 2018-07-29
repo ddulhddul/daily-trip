8c8c75d3c3426662ac05a49587233a77f68ac3ca
ddulh <ddulhddul@gmail.com>
2018-07-29 14:54:48 +0900

    add popup init

--- src/components/main/AddMainComponent.js
+++ src/components/main/AddMainComponent.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { View, Text, Button, Picker } from 'react-native';
+import { View, Picker } from 'react-native';
+import { DatePicker, Text, Button, Container, Content } from "native-base";
 import jsonData from './nation.json'
 
-export default class AddMainComponent extends React.Component {
+import {bindActionCreators} from 'redux';
+import { connect } from 'react-redux';
+
+import * as Actions from '../../actions'; //Import your actions
+
+class AddMainComponent extends React.Component {
   constructor(){
     super()
     this.state = {
-      nation: ''
+      nation: '',
+      startDate: new Date(),
+      endDate: new Date()
     }
   }
 
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 30 }}>여행 정보 입력</Text>
+        <Text style={{ fontSize: 30, padding: 30 }}>여행 정보 입력</Text>
         <View
           style={{
             flexDirection: 'row',
@@ -33,11 +41,73 @@ export default class AddMainComponent extends React.Component {
             </Picker>
           </View>
         </View>
-        <Button
-          onPress={() => this.props.navigation.goBack()}
-          title="Back"
-        />
+
+        <View
+        style={{
+          flexDirection: 'row',
+          height: 100,
+          padding: 20,
+        }}>
+          <View style={{flex:0.5}}>
+            <DatePicker
+              locale={"ko"}
+              formatChosenDate={(date)=>{
+                return [date.getFullYear(),date.getMonth()+1,date.getDate()].join('-')
+              }}
+              timeZoneOffsetInMinutes={undefined}
+              modalTransparent={false}
+              animationType={"fade"}
+              androidMode={"spinner"}
+              placeHolderText="시작 날짜"
+              textStyle={{ color: "green", textAlign: "center" }}
+              placeHolderTextStyle={{ color: "#d3d3d3", textAlign: "center" }}
+              onDateChange={(date)=>this.setDate({startDate:date})}
+            />
+          </View>
+          <View style={{flex:0.5}}>
+            <DatePicker
+              locale={"ko"}
+              formatChosenDate={(date)=>{
+                return [date.getFullYear(),date.getMonth()+1,date.getDate()].join('-')
+              }}
+              timeZoneOffsetInMinutes={undefined}
+              modalTransparent={false}
+              animationType={"fade"}
+              androidMode={"spinner"}
+              placeHolderText="종료 날짜"
+              textStyle={{ color: "green", textAlign: "center" }}
+              placeHolderTextStyle={{ color: "#d3d3d3", textAlign: "center" }}
+              onDateChange={(date)=>this.setDate({endDate:date})}
+            />
+          </View>
+        </View>
+        <View
+        style={{
+          flexDirection: 'row',
+          padding: 20,
+        }}>
+          <Button rounded success
+            onPress={() => {
+              this.props.insert({
+                title: this.state.nation,
+                contents: this.state.nation+'contents test'
+              })
+              this.props.navigation.goBack()
+            }}>
+            <Text>추가</Text>
+          </Button>
+          <Button rounded danger
+            onPress={() => this.props.navigation.goBack()}>
+            <Text>뒤로</Text>
+          </Button>
+        </View>
       </View>
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(Actions, dispatch);
+}
+
+export default connect(undefined, mapDispatchToProps)(AddMainComponent);
